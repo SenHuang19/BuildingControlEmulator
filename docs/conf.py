9ef95860325a7e0480c31e1d8ef9627f568caab7
Laurentiu Marinovici <laurmarinovici@gmail.com>
2019-07-31 15:46:41 -0700

    ==

--- docs/conf.py
+++ docs/conf.py
@@ -62,15 +62,15 @@ pygments_style = 'sphinx'
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_context = {
-    'css_files': [
-        '_static/custom_style.css',  # override wide tables in RTD theme
-        ],
-     }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_context = {
+    'css_files': [
+        '_static/custom_style.css',  # override wide tables in RTD theme
+        ],
+     }
 
 autodoc_mock_imports = ["flask", "flask_restful", "pyfmi", "numpy", "copy"]
